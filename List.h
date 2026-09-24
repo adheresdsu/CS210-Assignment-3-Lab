f6eb669842e1107300a83b8e9f7486fb013c42f1
Aryan Dhere <adhere6393@sdsu.edu>
2026-09-23 18:11:27 -0700

    Verify List ADT with ArrayList

--- List.h
+++ List.h
@@ -22,6 +22,6 @@ public:
 
 template <typename T>
 std::unique_ptr<List<T>> makeList() {
-    return std::make_unique<LinkedList<T>>();
-    // return std::make_unique<ArrayList<T>>();
+    // return std::make_unique<LinkedList<T>>();
+    return std::make_unique<ArrayList<T>>();
 }
